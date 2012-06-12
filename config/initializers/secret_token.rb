15977dd0a506bd79e30889d36c3b4eeb5d2fcdb0
Beyond <beyond@be.to>
2012-06-12 17:34:17 +0900

    rails 3.2.5

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-RailsAppSample::Application.config.secret_token = 'd276662d125c0fc283f5c3124a2c20145c1868cc3e31bcdf74da1ec52a1e3daf50b06c7c3bc31355527a2ef3b1615304881b6494d58a2b535a6bb6ab53f7ac1a'
+RailsAppSample::Application.config.secret_token = '32547be8889496504861e54cb9d0a3af5c019ab2e85d68deb02357ee0a1d2d332d2c022c0971524ac1972d848081f29b975f2fae0ef9daf98de2bd28b5df9988'
